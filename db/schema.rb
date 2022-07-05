8a2aeaa2ef144522d574eb225edc2565e906111a
ayaka-osoda <aytimn@gmail.com>
2022-07-05 23:22:55 +0900

    rails g model Task name:string user:references completed:boolean deadline:date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_04_153108) do
+ActiveRecord::Schema.define(version: 2022_07_05_141949) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.boolean "completed"
+    t.date "deadline"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,4 +29,5 @@ ActiveRecord::Schema.define(version: 2022_07_04_153108) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
