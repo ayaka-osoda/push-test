2d9f406643292a4f3f05be2b637c9376a296d324
ayaka-osoda <aytimn@gmail.com>
2022-07-08 00:23:23 +0900

    add update action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,11 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+    @task.completed = !@task.completed
+    @task.save
+    redirect_to tasks_url
+  end
 end
