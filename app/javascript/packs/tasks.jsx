a32bd5512d25a978d8ef748079fd96961879dc82
ayaka-osoda <aytimn@gmail.com>
2022-07-21 00:05:35 +0900

    manage tasks data by useState

--- app/javascript/packs/tasks.jsx
+++ app/javascript/packs/tasks.jsx
@@ -2,21 +2,34 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Tasks = (props) => (
-  <div className="todo-app">
-    <h1>Todo List</h1>
-    <ul className="todo-list">
-      {props.tasks.map(task => {
-        return <li key={task.id}>
-          <button>{task.completed ? "完了" : "未完了"}</button><span>{task.name}</span>
-          </li>;
-      })}
-    </ul>
-  </div>
-)
+const Tasks = (props) => {
+  const [state, setState] = useState({...props});
+
+  // FIXME: APIを呼び出してデータを変更する
+  const toggleCompleted = (id) => {
+    const newTasks = state.tasks.map(task => {
+      if (task.id === id) task.completed = !task.completed;
+      return task;
+    });
+    setState({ tasks: newTasks });
+  };
+
+  return (
+    <div className="todo-ap">
+      <h1>Todo List</h1>
+      <ul className="todo-list">
+        {state.tasks.map(task => {
+          return <li key={task.id}>
+              <button onClick={() => toggleCompleted(task.id)}>{task.completed ? "完了" : "未完了"}</button><span>{task.name}</span>
+            </li>;
+        })}
+      </ul>
+    </div>
+  )
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const node = document.getElementById('resources-container')
