e9e07b76030ee4c1e84040115e080397e8d850b9
ayaka-osoda <aytimn@gmail.com>
2022-08-23 07:57:07 +0900

    change packages for typescript compile

--- babel.config.js
+++ babel.config.js
@@ -42,7 +42,8 @@ module.exports = function (api) {
           development: isDevelopmentEnv || isTestEnv,
           useBuiltIns: true
         }
-      ]
+      ],
+      ['@babel/preset-typescript', { 'allExtensions': true, 'isTSX': true }]
     ].filter(Boolean),
     plugins: [
       'babel-plugin-macros',
